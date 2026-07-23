40d7e8657fc6abe751ccd813b211d4147ffa4169
Maic Lopez Saenz <maic@lopsae.com>
2026-07-22 19:34:35 -0700

    copy a gradient from frame-cutter

--- sources/ButtonIntricacies.swift
+++ sources/ButtonIntricacies.swift
@@ -9,96 +9,104 @@ import SwiftUI
 
 
 #Preview("Experiments") {
-    Button(
-        "Glass Regular",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .buttonStyle(.glass)
+    ZStack {
+        MeshGradient.wallOfIceAndFire
+        .ignoresSafeArea()
 
-    Button(
-        "Glass Prominent",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .buttonStyle(.glassProminent)
+        VStack {
+            Button(
+                "Glass Regular",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .buttonStyle(.glass)
 
-    Button(
-        "Capsule",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .buttonBorderShape(.capsule)
-    .buttonStyle(.glassProminent)
+            Button(
+                "Glass Prominent",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .buttonStyle(.glassProminent)
 
-    Button(
-        "Glass Prominent",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .labelStyle(.iconOnly)
-    .buttonBorderShape(.capsule)
-    .buttonStyle(.glassProminent)
+            Button(
+                "Capsule",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.glassProminent)
 
-    Button(
-        "Glass Prominent",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .labelStyle(.iconOnly)
-    .buttonBorderShape(.circle)
-    .buttonStyle(.glassProminent)
+            Button(
+                "Glass Prominent",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .labelStyle(.iconOnly)
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.glassProminent)
 
-    Button(
-        "Glass Prominent",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .labelStyle(.iconOnly)
-    .buttonStyle(.plain)
-    .padding(8)
-    .debugOverlay(.hairline)
-    .glassEffect(.regular.interactive())
+            Button(
+                "Glass Prominent",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .labelStyle(.iconOnly)
+            .buttonBorderShape(.circle)
+            .buttonStyle(.glassProminent)
 
-    Button(
-        "Glass Prominent",
-        systemImage: "envelope.badge.shield.half.filled",
-        action: {}
-    )
-    .labelStyle(.iconOnly)
-    .buttonStyle(.plain)
-    .frame(squareOf: 15, alignment: .centerLastTextBaseline)
-    .frame(squareOf: 44, alignment: .center)
-    .debugOverlay(.hairline)
-    .glassEffect(.regular.interactive())
+            Button(
+                "Glass Prominent",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .labelStyle(.iconOnly)
+            .buttonStyle(.plain)
+            .padding(8)
+            .debugOverlay(.hairline)
+            .glassEffect(.regular.interactive())
 
-    HStack(alignment: .firstTextBaseline) {
-        Label("Label", systemImage: "circle")
+            Button(
+                "Glass Prominent",
+                systemImage: "envelope.badge.shield.half.filled",
+                action: {}
+            )
+            .labelStyle(.iconOnly)
+            .buttonStyle(.plain)
+            .frame(squareOf: 15, alignment: .centerLastTextBaseline)
+            .frame(squareOf: 44, alignment: .center)
+            .debugOverlay(.hairline)
+            .glassEffect(.regular.interactive())
 
-        Button(
-            "Glass Prominent",
-            systemImage: "envelope.badge.shield.half.filled",
-            action: {}
-        )
-        .font(.title)
-        .labelStyle(.iconOnly)
-        .buttonStyle(.plain)
-        .frame(squareOf: 15, alignment: .centerLastTextBaseline)
-        .frame(squareOf: 44, alignment: .center)
-        .debugOverlay(.hairline)
-        .glassEffect(.regular.interactive())
+            HStack(alignment: .firstTextBaseline) {
+                Label("Label", systemImage: "circle")
 
-        Button(
-            "Glass Prominent",
-            systemImage: "envelope.badge.shield.half.filled",
-            action: {}
-        )
-        .font(.title)
-        .labelStyle(.iconOnly)
-        .buttonStyle(.plain)
-//                .frame(squareOf: 15, alignment: .centerLastTextBaseline)
-        .frame(squareOf: 44, alignment: .center)
-        .debugOverlay(.hairline)
-        .glassEffect(.regular.interactive())
+                Button(
+                    "Glass Prominent",
+                    systemImage: "envelope.badge.shield.half.filled",
+                    action: {}
+                )
+                .font(.title)
+                .labelStyle(.iconOnly)
+                .buttonStyle(.plain)
+                .frame(squareOf: 15, alignment: .centerLastTextBaseline)
+                .frame(squareOf: 44, alignment: .center)
+                .debugOverlay(.hairline)
+                .glassEffect(.regular.interactive())
+
+                Button(
+                    "Glass Prominent",
+                    systemImage: "envelope.badge.shield.half.filled",
+                    action: {}
+                )
+                .font(.title)
+                .labelStyle(.iconOnly)
+                .buttonStyle(.plain)
+        //                .frame(squareOf: 15, alignment: .centerLastTextBaseline)
+                .frame(squareOf: 44, alignment: .center)
+                .debugOverlay(.hairline)
+                .glassEffect(.regular.interactive())
+            }
+        }
     }
+
 }
