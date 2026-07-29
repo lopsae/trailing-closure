c93e3a808d6668b06c6d751085716e70c228f194
Maic Lopez Saenz <maic@lopsae.com>
2026-07-29 15:50:02 -0700

    add pristine app for illustrations

---
+++ illustrations-app/Illustration App/Illustration App/Illustration_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Illustration_AppApp.swift
+//  Illustration App
+//
+//  Created by Maic Lopez Saenz on 2026-07-29.
+//
+
+import SwiftUI
+
+@main
+struct Illustration_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ illustrations-app/Illustration App/Illustration AppTests/Illustration_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Illustration_AppTests.swift
+//  Illustration AppTests
+//
+//  Created by Maic Lopez Saenz on 2026-07-29.
+//
+
+import Testing
+@testable import Illustration_App
+
+struct Illustration_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
