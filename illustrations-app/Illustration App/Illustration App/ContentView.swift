ec96a032bfb3f1e1cdeea95ef0bfb470b672647f
Maic Lopez Saenz <maic@lopsae.com>
2026-07-29 15:50:02 -0700

    add pristine app for illustrations

---
+++ illustrations-app/Illustration App/Illustration App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Illustration App
+//
+//  Created by Maic Lopez Saenz on 2026-07-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
